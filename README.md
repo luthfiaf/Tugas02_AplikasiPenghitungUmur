f767e838962068af169d393d6ae11a152de0715c
muhammad.arfn26@gmail.com <muhammad.arfn26@gmail.com>
2024-10-30 18:39:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tugas02_AplikasiPenghitungUmur
+ Latihan 2 - Muhammad Luthfianur arifin (2210010213)
---
+++ src/tugas02_aplikasipenghitungumur/Tugas02_AplikasiPenghitungUmur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas02_aplikasipenghitungumur;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas02_AplikasiPenghitungUmur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
